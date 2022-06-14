40331fd49a46cf29f44bb48b7de773ddf8458f67
Christiaan Kras <c.kras@heerenveen.nl>
2022-06-14 13:37:32 +0200

    EUROPEAN UNION PUBLIC LICENCE v. 1.2

--- samenwerkende-catalogi.php
+++ samenwerkende-catalogi.php
@@ -1,14 +1,27 @@
 <?php
-/**
- * Plugin Name: Samenwerkende Catalogi
- * Plugin URI: http://www.heerenveen.nl/plugins/samenwerkende-catalogi/
- * Description: Plugin voor het creëren van een XML-feed voor de Samenwerkende Catalogi
- * Version: 1.1.2
- * Author: Gemeente Heerenveen
- * Author URI: https://www.heerenveen.nl
- * Requires at least: 3.0
- * Tested up to: 4.1.1
- */
+/*
+Plugin Name: Samenwerkende Catalogi
+Plugin URI: https://github.com/OpenWebconcept/plugin-samenwerkende-catalogi
+Description: Plugin voor het creëren van een XML-feed voor de Samenwerkende Catalogi
+Version: 1.1.2
+Requires at least: 3.0
+Tested up to: 4.1.1
+Author: Gemeente Heerenveen
+Author URI: https://www.heerenveen.nl
+License URI: https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+License: EUPL v1.2
+Copyright 2018-2022 Gemeente Heerenveen
+
+Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+
+Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Licence for the specific language governing permissions and limitations under the Licence.
+*/
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
